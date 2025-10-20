74d35bdcb93bd3c30da2a49cf98c2ce3fa4cd94f
Jorge Rangel <jorgerangel@192.168.1.6>
2025-10-20 06:27:43 -0600

    Move stack navigators to a new dir called navigators as in the future we will add more with different values

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,5 @@
 import { createStaticNavigation } from '@react-navigation/native'
-import RootStack from '@src/stacks/RootStack'
+import RootStack from '@src/navigators/RootStack'
 
 const Navigation = createStaticNavigation(RootStack)
 
